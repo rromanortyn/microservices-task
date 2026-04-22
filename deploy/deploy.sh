47b39a43bcff7925d658f1dc4bdd600df1ec2098
unknown <rromanortyn@gmail.com>
2026-04-22 18:29:24 +0300

    Test 2

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -19,13 +19,13 @@ sudo docker compose --help
 
 # sudo docker compose down || true
 
-# sudo touch .env
-# sudo chmod 666 .env
+sudo touch .env
+sudo chmod 666 .env
 
-# aws secretsmanager get-secret-value --secret-id microservices-task-env --query SecretString --output text > .env
+aws secretsmanager get-secret-value --secret-id microservices-task-env --query SecretString --output text > .env
 
-# sudo docker compose --env-file=.env -f docker/docker-compose.yml up -d
-# sudo docker image prune -f
+sudo docker compose --env-file=.env -f docker/docker-compose.yml up -d
+sudo docker image prune -f
 
 # echo "FIRST npm i"
 
