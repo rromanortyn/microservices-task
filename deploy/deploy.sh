cbbd9deac9e75a81f540208efe03e14357ddc266
unknown <rromanortyn@gmail.com>
2026-04-22 11:55:32 +0300

    Fixed deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 cd /home/ec2-user/microservices-task
 
-docker compose down || true
+sudo docker compose down || true
 
 sudo touch .env
 sudo chmod 666 .env
 
 aws secretsmanager get-secret-value --secret-id microservices-task-env --query SecretString --output text > .env
 
-docker compose --env-file=.env -f docker/docker-compose.yml up -d
-docker image prune -f
+sudo docker compose --env-file=.env -f docker/docker-compose.yml up -d
+sudo docker image prune -f
