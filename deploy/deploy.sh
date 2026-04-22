26400114e8108ec4195b0d51717d39428b610504
unknown <rromanortyn@gmail.com>
2026-04-22 11:31:28 +0300

    Added secrets

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 cd /home/ec2-user/microservices-task
 
 docker compose down || true
+
+aws secretsmanager get-secret-value --secret-id microservices-task-env --output text > .env
+
 docker compose --env-file=.env -f docker/docker-compose.yml up -d
 docker image prune -f
