9edc7ab241d9ead8de6af8b8248016033b7d9c81
unknown <rromanortyn@gmail.com>
2026-04-22 11:51:15 +0300

    Fixed .env loading

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ docker compose down || true
 sudo touch .env
 sudo chmod 666 .env
 
-aws secretsmanager get-secret-value --secret-id microservices-task-env --output text > .env
+aws secretsmanager get-secret-value --secret-id microservices-task-env --query SecretString --output text > .env
 
 docker compose --env-file=.env -f docker/docker-compose.yml up -d
 docker image prune -f
