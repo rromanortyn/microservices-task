3ac76f016fc0f7bccae71029c1d3636465076450
unknown <rromanortyn@gmail.com>
2026-04-22 11:35:16 +0300

    Added creation of .env in appspec.yml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,9 @@ cd /home/ec2-user/microservices-task
 
 docker compose down || true
 
+touch .env
+chmod 666 .env
+
 aws secretsmanager get-secret-value --secret-id microservices-task-env --output text > .env
 
 docker compose --env-file=.env -f docker/docker-compose.yml up -d
