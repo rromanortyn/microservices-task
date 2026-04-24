0f8fa07ea4b98c1c0bff3f085682f90ab000a0b1
unknown <rromanortyn@gmail.com>
2026-04-24 14:46:09 +0300

    Progress

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,10 +1,16 @@
+import path from 'node:path'
+import fs from 'node:fs'
+
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import AppModule from './modules/app/app.module'
 
 async function bootstrap() {
-  console.log(process.cwd())
+  const envPath = path.join(process.cwd(), 'user-service', '.env')
+  const env = fs.readFileSync(envPath, 'utf-8')
+  console.log('ENV:', env)
+
   const app = await NestFactory.create(AppModule)
   // const port = Number(process.env.PORT ?? 4001)
   const port = 0
