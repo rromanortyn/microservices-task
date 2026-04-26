4cc523d7b1d44e5f6294ae363d9764a69b13d781
unknown <rromanortyn@gmail.com>
2026-04-26 13:07:04 +0300

    Progress

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -7,7 +7,7 @@ sudo chmod 666 .env
 
 aws secretsmanager get-secret-value --secret-id microservices-task-env --query SecretString --output text > .env
 
-sudo docker compose --env-file=.env -f docker/docker-compose.yml up -d
+sudo -E docker compose --env-file=.env -f docker/docker-compose.yml up -d
 sudo docker image prune -f
 
 cd /home/ec2-user/microservices-task/user-service
