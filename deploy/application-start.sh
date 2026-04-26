9a827f7d6570ea21d11421810ecb2b7d41853d08
unknown <rromanortyn@gmail.com>
2026-04-26 12:26:34 +0300

    Progress

--- deploy/application-start.sh
+++ deploy/application-start.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd /home/ec2-user/microservices-task
 
-user_service_status=$(sudo -E pm2 jlist | jq -r '[.[] | select(.name == "user-service") | .pm2_env.status] | last')
+user_service_status=$(sudo -E pm2 jlist | jq -r 'first(.[] | select(.name == "user-service")) | .pm2_env.status')
 
 if [ "$user_service_status" = "online" ]; then
     user_service_is_online=true
@@ -13,5 +13,16 @@ fi
 
 echo "User service status: $user_service_status"
 echo "User service is online: $user_service_is_online"
+
+vehicle_service_status=$(sudo -E pm2 jlist | jq -r 'first(.[] | select(.name == "vehicle-service")) | .pm2_env.status')
+
+if [ "$vehicle_service_status" = "online" ]; then
+    vehicle_service_is_online=true
+else
+    vehicle_service_is_online=false
+fi
+
+echo "Vehicle service status: $vehicle_service_status"
+echo "Vehicle service is online: $vehicle_service_is_online"
 
 # sudo npm run dev
