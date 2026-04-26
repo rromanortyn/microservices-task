b45924cceb6be31dd296e14baa8a068db8b673c1
unknown <rromanortyn@gmail.com>
2026-04-26 12:00:01 +0300

    Progress

--- deploy/application-start.sh
+++ deploy/application-start.sh
@@ -2,4 +2,16 @@
 set -euo pipefail
 
 cd /home/ec2-user/microservices-task
-sudo npm run dev
+
+user_service_status=$(pm2 jlist | jq -r '.[] | select(.name == "user-service") | .pm2_env.status')
+
+if [ "$user_service_status" = "online" ]; then
+    user_service_is_online=true
+else
+    user_service_is_online=false
+fi
+
+echo "User service status: $user_service_status"
+echo "User service is online: $user_service_is_online"
+
+# sudo npm run dev
